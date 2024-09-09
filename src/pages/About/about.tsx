66930302744366b1ed2f8ef3e39c75b17feffd7f
zhonions <84675944+zhonions@users.noreply.github.com>
2024-09-09 03:15:25 +0100

    add content in page about

--- src/pages/About/about.tsx
+++ src/pages/About/about.tsx
@@ -1,12 +1,43 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Slideshow from "../../components/SlideShow/SlideShow";
-
+import logo from "../../assets/images/logo.svg"
+import "./about.scss"
 const About = () => {
   return (
     <>
       <Slideshow/>
       <Navbar />
-      
+      <section className="container">
+        <div className="align-content-center">
+          <div className="about-us-content">
+            <h2>
+              From Gamers to <span>Gamers</span>
+            </h2>
+            <div className="about-us-description">
+            Ola eu sou o paulo
+            </div>
+          </div>
+          <div>
+            <img
+              className="logo"
+              src={logo}
+              alt="mancos Gaming"
+            />
+          </div>
+        </div>
+      </section>
+      <div className="back-color">
+        <section className="container2">
+          <div className="align-content-center2">
+            <div className="about-us-content2">
+              <h2>What we give Players</h2>
+              <div className="about-us-description">
+            Ola eu sou o paulo
+            </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
