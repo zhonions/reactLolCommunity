0210edcde259a19ab89a532dba314e1a7823dfdf
zhonions <84675944+zhonions@users.noreply.github.com>
2024-09-09 14:49:24 +0100

    refactor gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import sass from "sass";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/reactLolCommunity",
   css: {
     preprocessorOptions: {
       scss: {
